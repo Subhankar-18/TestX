4eaf166f44b21f84c725e314b1132254a7023d61
ShreyasiSadhukhan <shreyasisadhukhan4@gmail.com>
2025-09-16 20:02:23 +0530

    frontend

--- backend/src/main/java/com/example/demo/controller/CategoryController.java
+++ backend/src/main/java/com/example/demo/controller/CategoryController.java
@@ -17,8 +17,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
 
-
-
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/category")
--- frontend/src/Normalpage.jsx
+++ frontend/src/Normalpage.jsx
@@ -5,7 +5,7 @@ const Normalpage = () => {
   return (
     <div>
       <h2>Normal User Page</h2>
-      <p>Welcome, User!</p>
+      <p>Welcome, User! user is normal type rendered properly</p>
       {/* Normal user-specific content */}
     </div>
   );
